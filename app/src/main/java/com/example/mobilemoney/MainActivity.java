9543077c8620760b592b275104e37247b4094999
DAVID KATHOH <mudavidkat@gmail.com>
2019-10-26 15:05:00 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mobilemoney/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mobilemoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.hover.sdk.api.Hover;
+import com.hover.sdk.api.HoverParameters;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO(4) Initialise hover here
+        Hover.initialize(this);
+
+        Button buy = findViewById(R.id.btn_buy);
+
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            Intent intent = new HoverParameters.Builder(getApplicationContext())
+                    .request("action_id")
+                    .extra("montat","40")
+                    .extra("beneficiare","0972062484")
+                    .buildIntent();
+            startActivityForResult(intent,3);
+
+            }
+        });
+    }
+}
